68bc1761326a149bdf81a20f0c6df7beb06e1167
Vikram Baliga <vikkubaligatmp@gmail.com>
2023-05-05 14:38:36 +0530

    new project structure with sequalise

--- app.js
+++ app.js
@@ -1,61 +1,16 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("der_v3", "dvr", "dvr2022", {
-  host: "localhost",
-  dialect: "mysql",
-  operatorsAliases: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-
-const usersRouter = require('./users');
-
-const app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use('/dvr', usersRouter);
-
-
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+const bodyParser = require('body-parser');
 const http = require('http');
-app.set('port','3000');
-
+const app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('*', (req, res) => res.status(200).send({
+      message: 'Welcome to the beginning of nothingness.',
+}));
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
 const server = http.createServer(app);
-
-app.listen(app.get('port'), () => {
-  console.log(`Express server listening on port ${app.get('port')}`);
-})
-
+server.listen(port);
+module.exports = app;
